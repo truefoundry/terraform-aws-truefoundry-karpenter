878537a7ef12806a2e185bf4da8faa6eeb2e935e
Vedant Pareek <dunefro@gmail.com>
2025-04-04 23:01:25 +0530

    Fixing count in output

--- output.tf
+++ output.tf
@@ -4,7 +4,7 @@ output "karpenter_role_arn" {
 }
 
 output "karpenter_instance_profile_id" {
-  value       = var.create_karpenter_iam_role ? aws_iam_instance_profile.karpenter.id : ""
+  value       = var.create_karpenter_iam_role ? aws_iam_instance_profile.karpenter[0].id : ""
   description = "Karpenter instance profile ID"
 }
 
